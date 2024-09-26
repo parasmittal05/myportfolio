8bdaf850e04a6ad6fa6e75a57c94da4c9eb88635
paras mittal <mittalparas588@gmail.com>
2024-09-26 13:04:25 +0530

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="parasmittal8484@gmail.com">parasmittal8484@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/paras-mittal-5aa791228/">https://www.linkedin.com/in/paras-mittal-5aa791228/</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/parasmittal05">https://github.com/parasmittal05</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
